56d227f79a3381e07ef51201dcacb56e963f574a
Alexandra Ungur <ungur.alexandra.au@gmail.com>
2021-11-01 16:04:41 +0200

    adaugare eventListeners pe fiecare buton

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-var random_number = Math.floor(Math.random() * 3); //random number
-//console.log(random_number);
+var random_number = Math.floor(Math.random() * 3); //random number generated
+console.log("Numarul castigator este: " + random_number); //afisare numar random
+var user_number = document.getElementsByClassName("btn").value; //valoarea butonului clickuit de user
+
+document
+  .getElementsByClassName("btn")
+  .addEventListener("click", displayMessage); //actiunea de pe butonul clickuit
+
+function displayMessage() {
+  //functia de verificare daca numarul generat e egal cu cel de pe butonul ales
+  if (random_number == user_number) {
+    alert("You guessed right");
+  } else {
+    alert("You guessed wrong");
+  }
+}
