c3483d35e52f30e4c4c88315858ae433faaf448f
Alexandra Ungur <68352666+AleUngur@users.noreply.github.com>
2021-11-02 15:59:06 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-var random_number = generateRandomNumber(1, 3); //random number generated
-console.log("Winning number is: " + random_number); //display in console the generated number
+var random_number = generateRandomNumber(1, 3);
+//console.log("Winning number is: " + random_number);
 
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function displayMessage(idBtnClicked) {
-  //checks if the clicked button is the same with the random number
   if (random_number == idBtnClicked) {
     alert("Congratulations! You guessed right");
   } else {
