610466a0bb1ddb96e583546a5bdddff0828e31f2
Alexandra Ungur <ungur.alexandra.au@gmail.com>
2021-11-02 11:27:57 +0200

    changed the code to work

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 var random_number = Math.floor(Math.random() * 3); //random number generated
-console.log("Numarul castigator este: " + random_number); //afisare numar random
-var user_number = document.getElementsByClassName("btn").value; //valoarea butonului clickuit de user
+console.log("Winning number is: " + random_number); //display in console the generated number
 
-document
-  .getElementsByClassName("btn")
-  .addEventListener("click", displayMessage); //actiunea de pe butonul clickuit
-
-function displayMessage() {
-  //functia de verificare daca numarul generat e egal cu cel de pe butonul ales
-  if (random_number == user_number) {
+function displayMessage(clicked_btn_id) {
+  //checks if the clicked button is the same with the random number
+  if (random_number == clicked_btn_id) {
     alert("You guessed right");
   } else {
     alert("You guessed wrong");
   }
 }
+
+//the interval of the random number is [0,3]
