4e6e928837f297f415768d4724de56651465808b
Alexandra Ungur <ungur.alexandra.au@gmail.com>
2021-11-05 20:35:23 +0200

    stored the user's guess + changed the random number method

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-var random_number = generateRandomNumber(1, 3);
-//console.log("Winning number is: " + random_number);
+document.getElementById("submitguess").onclick = function () {
+  user_number = document.getElementById("guessField").value; //number guessed by user
+  console.log("User's guess: " + user_number);
+  var random_number = generateRandomNumber(1, user_number); //number generated randomly
+  console.log("Winning number is: " + random_number);
 
-function generateRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+};
+
+function createButtons() {
+  for (var i = 1; i <= user_number; ++i) {}
 }
 
 function displayMessage(idBtnClicked) {
