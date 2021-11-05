36ae3b1158945966d72fec6ac3976aeb582d2247
Alexandra Ungur <ungur.alexandra.au@gmail.com>
2021-11-05 23:45:21 +0200

    made a function to create n buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("submitNumber").onclick = function () {
-  input_number = document.getElementById("numberField").value; //number guessed by user
+  input_number = document.getElementById("numberField").value; //number of buttons to be created
   console.log("User's number: " + input_number);
   var random_number = generateRandomNumber(1, input_number); //number generated randomly
   console.log("Winning number is: " + random_number);
@@ -8,14 +8,20 @@ document.getElementById("submitNumber").onclick = function () {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  function createButtons() {
-    for (var i = 1; i <= input_number; ++i) {
-      var button = document.createElement("button");
+  createButtons(input_number);
+
+  function createButtons(x) {
+    for (var i = 1; i <= x; ++i) {
+      var btn = document.createElement(
+        `<button type="button" class="btn btn-outline-success" style="width: 50px; height: 50px"></button>`
+      );
+      var body = document.getElementsByClassName("container")[i];
+      body.appendChild(button);
     }
   }
 
   function displayMessage(idBtnClicked) {
-    if (user_number == idBtnClicked) {
+    if (random_number == idBtnClicked) {
       alert("Congratulations! You guessed right");
     } else {
       alert("You guessed wrong");
