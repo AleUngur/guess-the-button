0147c7da1d5ec2fe66ec2a71c63508bb4e3f5737
Alexandra Ungur <ungur.alexandra.au@gmail.com>
2021-11-05 20:43:58 +0200

    updated script.js

--- script.js
+++ script.js
@@ -7,16 +7,18 @@ document.getElementById("submitguess").onclick = function () {
   function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
-};
 
-function createButtons() {
-  for (var i = 1; i <= user_number; ++i) {}
-}
-
-function displayMessage(idBtnClicked) {
-  if (random_number == idBtnClicked) {
-    alert("Congratulations! You guessed right");
-  } else {
-    alert("You guessed wrong");
+  function createButtons() {
+    for (var i = 1; i <= user_number; ++i) {
+      var button = document.createElement("button");
+    }
   }
-}
+
+  function displayMessage(idBtnClicked) {
+    if (user_number == idBtnClicked) {
+      alert("Congratulations! You guessed right");
+    } else {
+      alert("You guessed wrong");
+    }
+  }
+};
