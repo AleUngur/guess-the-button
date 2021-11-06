5baf2ee613faad9fd4a5e40515006f3d420b58de
Alexandra Ungur <ungur.alexandra.au@gmail.com>
2021-11-06 10:42:07 +0200

    made the createButtons method work

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("submitNumber").onclick = function () {
-  input_number = document.getElementById("numberField").value; //number of buttons to be created
+  var input_number = document.getElementById("numberField").value; //number of buttons to be created
   console.log("User's number: " + input_number);
   var random_number = generateRandomNumber(1, input_number); //number generated randomly
   console.log("Winning number is: " + random_number);
@@ -8,23 +8,28 @@ document.getElementById("submitNumber").onclick = function () {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
+  let i = 1;
+  let container = document.getElementsByClassName("container")[0];
+
+  function createButtons(n) {
+    for (i = 1; i <= n; ++i) {
+      let button = document.createElement("button");
+      button.className = "btn btn-success my-3 mx-1";
+      button.innerHTML = i;
+      button.type = "button";
+      button.id = i;
+      button.style = "width: 50px; height: 50px";
+      //button.onclick = displayMessage(this.id);
+      container.appendChild(button);
+    }
+  }
   createButtons(input_number);
-
-  function createButtons(x) {
-    for (var i = 1; i <= x; ++i) {
-      var btn = document.createElement(
-        `<button type="button" class="btn btn-outline-success" style="width: 50px; height: 50px"></button>`
-      );
-      var body = document.getElementsByClassName("container")[i];
-      body.appendChild(button);
-    }
-  }
-
-  function displayMessage(idBtnClicked) {
-    if (random_number == idBtnClicked) {
-      alert("Congratulations! You guessed right");
-    } else {
-      alert("You guessed wrong");
-    }
-  }
 };
+
+function displayMessage(idBtnClicked) {
+  if (random_number == idBtnClicked) {
+    alert("Congratulations! You guessed right");
+  } else {
+    alert("You guessed wrong");
+  }
+}
